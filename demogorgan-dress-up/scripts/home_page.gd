590acaf4d23a043f397011b388c7d08870363ca8
fishygaminglife-del <fishygaming.life@gmail.com>
2026-01-03 10:04:57 -0600

    Made full home page, instructions, credit, and end screen.

---
+++ demogorgan-dress-up/scripts/home_page.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$VolumeOff.visible = false
+	$VolumeOff.disabled = true
+	$Play.visible = false
+
+
+
+func _on_play_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/lvl1.tscn")
+	
+
+
+
+func _on_instruction_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/instructions.tscn")
+
+
+
+func _on_credit_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/credit.tscn")
+
+
+
+func _on_volume_on_pressed() -> void:
+	pass # Replace with function body.
+
+
+
+func _on_volume_off_pressed() -> void:
+	pass # Replace with function body.
